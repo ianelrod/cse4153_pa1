875d91a280a9a7a05269f910a0aa1c676a60b6c9
Ian Goforth <ian.goforth@gmail.com>
2022-02-14 13:46:07 -0600

    Commented out structure

--- src/client.java
+++ src/client.java
@@ -1,6 +1,37 @@
+// Name: Ian Goforth
+// Email: img56@msstate.edu
+// Student ID: 902-268-372
+
 public class client {
-    public static void main(String[] args)
+    public static void main(String[] args) // args include serverip, n_port, file
     {
         System.out.print("Hello World.");
+        // negotiation();
+
+        /* for loop to iterate
+        extract();
+        transaction();
+        if finished, send EOF
+        wait for server to acknowledge
+        close ports
+        */
+
+        // terminate
+    }
+    int negotiation(String serverip, String n_port) {
+        // send the characters 1248 to initiate a handshake
+        // receive r_port between 1024 and 65535 from server
+        // once handshake complete, close negotiation socket
+        return 0;
+    }
+    String extract(Object file, int position) {
+        String chars = "";
+        // extract 4 characters from file and return
+        return chars;
+    }
+    void transaction(String chars, String serverip, String r_port) {
+        // pack 4 characters into UDP packet
+        // send over random port and serverip
+        // send EOF at end of file
     }
 }
--- src/server.java
+++ src/server.java
@@ -1,6 +1,45 @@
+// Name: Ian Goforth
+// Email: img56@msstate.edu
+// Student ID: 902-268-372
+
 public class server {
-    public static void main(String[] args)
+    public static void main(String[] args) // args include n_port
     {
         System.out.print("Hello World.");
+        // negotiation();
+
+        /* for loop to iterate
+        extract();
+        transaction();
+        chars -> concat after each loop
+        receive EOF, send last acknowledgement
+        close ports
+         */
+
+        // write();
+        // terminate
+    }
+    int negotiation(int n_port) {
+        int r_port = 0;
+        // receive characters 1248 from client for handshake
+        // send random port between 1024 and 65535 to client
+        // write to screen: "Random port chosen: <r_port>"
+        // once handshake complete, close negotiation socket
+        return r_port;
+    }
+    String transaction(int r_port) {
+        String chars = "";
+        // use random port and serverip to receive file in UDP packets
+        // each UDP packet contains chunks of 4 characters
+        // acknowledge after each packet with payload in capital letters
+        // wait for EOF
+        return chars;
+    }
+    Object write(String concat) {
+        Object upload = null;
+        // Check if upload.txt exists
+        // if exists, delete
+        // write to upload.txt
+        return upload;
     }
 }
